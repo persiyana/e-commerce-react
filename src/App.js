7bc08b84a46952e6fc17bff3834b0173c9d7377e
Persiyana <persiyanageorgieva@gmail.com>
2022-08-28 18:37:22 +0300

    showing only 20 random products on the page

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import './App.css';
 import React, { useState, useEffect } from "react";
 import apiKey from './asos-apiKey';
 
+
+
 function App() {
   const [products, setProducts] = useState([]);
   const [category, setCategory] = useState([]);
+
+  let indexes = [];
+  let productsTwenty = [];
 
   const options = {
     method: 'GET',
@@ -13,7 +18,6 @@ function App() {
       'X-RapidAPI-Host': 'asos2.p.rapidapi.com'
     }
   };
-
   const url = 'https://asos2.p.rapidapi.com/products/v2/list?store=US&offset=0&categoryId=4209&limit=48&country=US&sort=freshness&currency=USD&sizeSchema=US&lang=en-US';
 
   const fetchProducts = async () => {
@@ -21,15 +25,37 @@ function App() {
     const info = await response.json();
     const category = info.categoryName;
     const products = info.products;
+    
+    getRandomIndexes(products.length);
+    getTwentyProducts(products);
+
     setCategory(category);
-    setProducts(products);
-    console.log(products);
+    setProducts(productsTwenty);
+
   };
+
+  const getRandomIndexes = (numOfProducts) => {
+      while (indexes.length < 20) {
+          const index = Math.floor(Math.random() * numOfProducts);
+          if (!indexes.includes(index)) {
+              indexes.push(index);
+              console.log(index);
+          }
+      }
+  }
+  const getTwentyProducts = (products) => {
+    for (let index = 0; index < 20; index++) {
+
+      productsTwenty[index]=products[indexes[index]];
+      console.log(productsTwenty[index]);
+      
+    }
+  }
 
   useEffect(() => {
     fetchProducts();
   }, []);
-
+  
   return (
     <main>
       <h2 id="categoryName">&gt;&gt; {category}</h2>
@@ -45,7 +71,11 @@ function App() {
     </main>
     
   );
+
 }
+
+
+
 
 export default App;
 
